ebbc5e50684d8ecd27b573066bf763ad94888998
Deepak Kumar Ravi <dpakravi93@gmail.com>
2023-02-21 23:27:25 +0530

    updating and adding books

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { isAuthenticated } from '../redux/Actions/auth';
 import { useDispatch } from 'react-redux';
 import { addItem, updateItem, removeItem } from '../redux/Actions/cart';
+import BookForm from './BookForm';
 function Book(props) {
   const { book, onDelete } = props;
+  const [edit, setEdit] = useState(false);
   const [qty, setQty] = useState(0);
   const dispatch = useDispatch();
   const handleAddToCart = () => {
@@ -14,28 +16,34 @@ function Book(props) {
     setQty(el => el + change);
     // (qty === 0) ? dispatch(removeItem(book._id)) : dispatch(updateItem({ ...book, quantity: (qty + change) }));
   }
-  useEffect(()=>{
+  useEffect(() => {
     (qty === 0) ? dispatch(removeItem(book._id)) : dispatch(updateItem({ ...book, quantity: qty }));
-  },[qty])
+  }, [qty])
 
   return (
     <div key={book._id} style={{ margin: "10px" }} className="col">
-      <div className="card">
-        <img src={book.image} className="card-img-top" height={100} alt="image" />
-        <div className="card-body">
-          <h5 className="card-title">{book.title}</h5>
-          <p>Author: {book.author}</p>
-          <p>Price: ₹ {book.price}</p>
-        </div>
-        {
-          isAuthenticated() && <div className='d-flex align-items-center gap-2'>
-            <button className="btn btn-outline-danger" onClick={() => onDelete(book._id)}>Delete</button>
-            {qty === 0 ? <button  className="btn btn-outline-success" onClick={handleAddToCart}>Add to cart</button> : (<div className='d-flex align-items-center gap-2'>
-              <button className="btn btn-outline-success" disabled={qty === 0 ? true : false} onClick={() => handleUpdateCart(-1)}>-</button><p>{qty}</p><button className="btn btn-outline-success" onClick={() => handleUpdateCart(1)}>+</button>
-            </div>)}
+      {edit ?
+        <BookForm book={book} /> : (
+          <div className="card">
+            <img src={book.image} className="card-img-top" height={100} alt="image" />
+            <div className="card-body">
+              <span className='d-flex justify-content-space-between gap-5'>
+              <h5 className="card-title">{book.title}</h5>
+              <img onClick={() => setEdit(!edit)} width={20} height={20} src="https://www.freeiconspng.com/thumbs/edit-icon-png/edit-editor-pen-pencil-write-icon--4.png" alt="edit" />
+              </span>
+              <p>Author: {book.author}</p>
+              <p>Price: ₹ {book.price}</p>
+            </div>
+            {
+              isAuthenticated() && <div className='d-flex flex-direction-column align-items-center gap-2'>
+                <button className="btn btn-outline-danger" onClick={() => onDelete(book._id)}>Delete</button>
+                {qty === 0 ? <button className="btn btn-outline-success" onClick={handleAddToCart}>Add to cart</button> : (<div className='d-flex align-items-center gap-2'>
+                  <button className="btn btn-outline-success" disabled={qty === 0 ? true : false} onClick={() => handleUpdateCart(-1)}>-</button><p>{qty}</p><button className="btn btn-outline-success" onClick={() => handleUpdateCart(1)}>+</button>
+                </div>)}
+              </div>
+            }
           </div>
-        }
-      </div>
+        )}
     </div>
   );
 }
--- client/src/components/BookForm.jsx
+++ client/src/components/BookForm.jsx
@@ -3,10 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 
 function BookForm({ book }) {
   const dispatch = useDispatch();
-  const products = useSelector((state) => state.books);
+  const products = useSelector((state) => state.books.books);
 
   const [title, setTitle] = useState(book ? book.title : '');
   const [author, setAuthor] = useState(book ? book.author : '');
+  const [price, setPrice] = useState(book ? book.price : 0);
+  const [image, setImage] = useState(book ? book.image : 0);
+  const [description, setDescription] = useState(book ? book.description : 0);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -43,6 +46,9 @@ function BookForm({ book }) {
     <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
       <input type="text" placeholder="Author" value={author} onChange={e => setAuthor(e.target.value)} />
+      <input type="number" placeholder="Price" value={price} onChange={e => setPrice(e.target.value)} />
+      <input type="text" placeholder="Imageurl" value={image} onChange={e => setPrice(e.target.value)} />
+      <textarea type="text" placeholder="Description" value={description} onChange={e => setPrice(e.target.value)} />
       <button type="submit">{book ? 'Update' : 'Add'}</button>
     </form>
   );
